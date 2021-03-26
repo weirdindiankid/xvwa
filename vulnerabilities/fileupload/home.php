bb0c49213ba398df945d54b058c762efd555e7a1
Dharmesh Tarapore <dharmesh@bu.edu>
2021-03-26 08:34:25 -0400

    Minor cleanup.

--- vulnerabilities/fileupload/home.php
+++ vulnerabilities/fileupload/home.php
@@ -1,14 +1,12 @@
- 
-
 <div class="thumbnail">
     <!--
         <img class="img-responsive" src="http://placehold.it/800x300" alt="">
       -->
       <div class="caption-full">
         <h4><a href="#">Unrestricted File Upload </a></h4>
-        
+
         <p align="justify">
-        As the name depicts, this issues happens when application does not validate file that is being uploaded by user. An attacker can upload malicious files called webshells on the server that could leads to complete server compromise. 
+        As the name depicts, this issues happens when application does not validate file that is being uploaded by user. An attacker can upload malicious files called webshells on the server that could leads to complete server compromise.
         </p>
         <p>Read more about Unrestricted File Upload<br>
         <strong><a target="_blank" href="https://www.owasp.org/index.php/Unrestricted_File_Upload">https://www.owasp.org/index.php/Unrestricted_File_Upload</a></p></strong>
@@ -18,34 +16,34 @@
 
       <div class="well">
         <table width="100%" style="border-collapse:collapse; table-layout:fixed;"><tr><td>
-          <div class="col-lg-12"> 
+          <div class="col-lg-12">
             <p><h4>Add New Item to the Coffee List</h4><br>
               <form method='post' action='' enctype="multipart/form-data">
-                <div class="form-group"> 
+                <div class="form-group">
                   <label></label>
                   <span class="file-input btn btn-primary btn-file">
                    Upload Image<input type="file" name="image">
                  </span>
                  <br><br>
-                 <input class="form-control" width="50%" placeholder="Item Name" name="item"></input> <br>
-                 <textarea class="form-control" placeholder="Description" rows="3" name="desc"></textarea><br>
-                 <input class="form-control" width="50%" placeholder="Category" name="categ"></input> <br>
-                 <input class="form-control" width="50%" placeholder="Price" name="price"></input> <br>
+                   <input class="form-control" width="50%" placeholder="Item Name" name="item" required="required"/><br>
+                 <textarea class="form-control" placeholder="Description" rows="3" name="desc" required="required"></textarea><br>
+                 <input class="form-control" width="50%" placeholder="Category" name="categ" required="required"/> <br>
+                 <input class="form-control" width="50%" placeholder="Price" name="price" required="required"/><br/>
 
                  <div align="right"> <button class="btn btn-default" type="submit">Submit Button</button></div>
 
-                 <br>
+                 <br/>
                </div>
              </form>
            </p>
          </div>
        </td>
        <td>
-        <div class="col-lg-12"> 
+        <div class="col-lg-12">
           <p><h4></h4><br>
 
-            <?php 
-            
+            <?php
+
             error_reporting(E_ALL);
 
             $itemcode = "XVWA".rand(1000,9999);
@@ -54,23 +52,21 @@
             $categ = isset($_POST['categ']) ? $_POST['categ'] : '';
             $price = isset($_POST['price']) ? $_POST['price']: '';
             $image = isset($_POST['image']);
-
-            if($itemname!='' && $itemdesc!='' && $categ!='' && $price!='' && basename( $_FILES['image']['name'])!=''){
+             if($itemname!='' && $itemdesc!='' && $categ!='' && $price!='' && basename( $_FILES['image']['name'])!=''){
 
               include('../../config.php');
 
               if(!$conn1){
                 echo "Error in connecting to database";
               }else{
-
               //uploading file
                 $path = $_SERVER['DOCUMENT_ROOT'].'/xvwa/img/uploads/';
-                $path = $path . basename( $_FILES['image']['name']); 
+                $path = $path . basename( $_FILES['image']['name']);
                 $rpath = '/xvwa/img/uploads/'.basename( $_FILES['image']['name']);
-		if(!move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
+                if(!move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
                   echo "<h4><b><font color='red'>There was an error uploading the file, please try again!</font></b></h4>";
                 }else{
-                  
+
                   $stmt = $conn1->prepare("INSERT INTO caffaine (itemcode, itemname, itemdisplay, itemdesc, categ, price) VALUES (:itemcode, :itemname, :itemdisplay, :itemdesc, :categ, :price)");
                   $stmt->bindParam(':itemcode', $itemcode);
                   $stmt->bindParam(':itemname', $itemname);
@@ -82,7 +78,7 @@
                   $sql = "select itemname,itemdisplay,itemdesc,categ,price from caffaine where itemcode = :itemcode";
                   $stmt = $conn1->prepare($sql);
                   $stmt->bindParam(':itemcode',$itemcode);
-                  $stmt->execute();
+                   $stmt->execute();
                   echo "<h4><b><font color='green'>Item Uploaded Successfully !!</font></b></h4><br>";
                   echo "<table>";
                   while($rows = $stmt->fetch(PDO::FETCH_NUM)){
@@ -90,23 +86,24 @@
                     echo "<tr><td><b>Name : </b>".htmlspecialchars($rows[0])."</td></tr>";
                     echo "<td><img src='".htmlspecialchars($rows[1])."' height=130 weight=20/></td>";
                     echo "<tr><td><b>Category : </b>".htmlspecialchars($rows[3])."</td></tr>";
-                    echo "<tr><td><b>Price : </b>".htmlspecialchars($rows[4])."$</td></tr>"; 
+                    echo "<tr><td><b>Price : </b>".htmlspecialchars($rows[4])."$</td></tr>";
                   }
                   echo "</table>";
 
 
                   // item uploaded
 
-                } 
+                }
               }
             }else{
               //enter full information 
+               echo "<h4><b><font color='red'>There was an error uploading the file, please try again and make sure all fields are filled.</font></b></h4>";
             }
             ?>
           </p>
         </div>
       </td></tr></table>
       <hr>
-      
+
     </div>
-    <?php include_once('../../about.html'); ?>
+                      <?php include_once('../../about.html'); ?>
